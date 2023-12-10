0af91d61f6be4d107533e1999c8041ab49ab327c
mohammadjavad <hossinbabayy1382@gmail.com>
2023-12-10 11:47:10 +0330

    my fields

--- main.go
+++ main.go
@@ -2,40 +2,18 @@ package main
 
 import "fmt"
 
-//Package scope array definition
-var integerSlice []int
-
-var stringSlice []string
-
-func main() {
-
-	integerSlice = []int{10, 20, 30, 40}
-
-	fmt.Println("This is the integer Slice: ", integerSlice)
-
-	stringSlice = []string{"first", "second", "third"}
-
-	fmt.Println("This is the string slice: ", stringSlice)
-
-	printInteger()
-
-	s := []int{10, 20, 30, 40}
-	fmt.Println("len(s) :", len(s), "cap(s) : ", cap(s))
-	fmt.Println(s[:])
-
-
-	for key, value := range s {
-	fmt.Println(key, value)
-	}
-
-	fmt.Println("======================")
-
-	for i := 0; 1< len(s); i++ {
-		fmt.Println(i,s[i])
-	}
+type person struct {
+	name string
+	family string
+	age int
 }
 
-//This function can access the integerSlice because integerSlice is package scoped
-func printInteger() {
-	fmt.Println("Print the integers: ", integerSlice)
+func main() {
+	var p person
+
+	p.age = 30
+	p.family = "babayy"
+	p.name = "mohammadjavad"
+
+	fmt.Println(p)
 }
